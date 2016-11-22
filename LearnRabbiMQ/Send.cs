3ffc25aa18f57001761bbe5848784d5f8b0686e1
taohonggou <cl889521@163.com>
2016-11-22 17:54:15 +0800

    开始学习demo

---
+++ LearnRabbiMQ/Send.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnRabbiMQ
+{
+    class Send
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localHost" };
+            using (var connection=factory.CreateConnection())
+            {
+                using (var channel=connection.CreateModel())
+                {
+
+                }
+            }
+        }
+    }
+}
