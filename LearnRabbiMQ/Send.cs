990de20685cca21dc57c13a6cea0ffafba01d0c2
taohonggou <cl889521@163.com>
2016-11-22 22:06:36 +0800

    写完了send.cs,但是不知道对不对

--- LearnRabbiMQ/Send.cs
+++ LearnRabbiMQ/Send.cs
@@ -12,13 +12,27 @@ namespace LearnRabbiMQ
         static void Main()
         {
             var factory = new ConnectionFactory() { HostName = "localHost" };
-            using (var connection=factory.CreateConnection())
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
-                using (var channel=connection.CreateModel())
-                {
-                    //http://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
-                }
+                //http://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+                channel.QueueDeclare(queue: "Hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+                string message = "Hello World";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", routingKey: "Hello", basicProperties: null, body: body);
+
+                Console.WriteLine("[X] send {0}", message);
+
             }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
         }
     }
 }
