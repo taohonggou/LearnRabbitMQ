f6ae8292b0d87eebbcbc0befb22b98040b4970a8
taohonggou <cl889521@163.com>
2016-11-23 10:08:02 +0800

    修改了文件件的名字

--- LearnRabbitMQ.Receive/Receive.cs
+++ LearnRabbitMQ.Receive/Receive.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace LearnRabbitMQ.Receive
 {
@@ -10,7 +12,24 @@ namespace LearnRabbitMQ.Receive
     {
         public static void Main()
         {
+            var factory = new ConnectionFactory() { HostName = "localHost" };
+            using (var connect=factory.CreateConnection())
+            using (var channel = connect.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[X] received {0}", message);
+                };
+                channel.BasicConsume(queue: "Hello", noAck: true, consumer: consumer);
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }
         }
     }
 }
