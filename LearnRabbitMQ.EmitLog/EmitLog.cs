3d0cbf5089a8c8d417fb805f5e17ee3610c99385
taohonggou <cl889521@163.com>
2016-11-23 18:11:03 +0800

    学完了发布订阅

---
+++ LearnRabbitMQ.EmitLog/EmitLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace LearnRabbitMQ.EmitLog
+{
+    class EmitLog
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("输入日志内容");
+                string message = Console.ReadLine();
+                if (message == "exit")
+                    return;
+                
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("logs", "fanout");
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+                    Console.WriteLine("[x] sent {0}",message);
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ LearnRabbitMQ.ReceiveLog/ReceiveLogs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace LearnRabbitMQ.ReceiveLog
+{
+    class ReceiveLogs
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("logs", "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,exchange: "logs", routingKey: "");
+
+                Console.WriteLine("Waiting for logs……");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] {0}",message);
+                };
+
+                channel.BasicConsume(queue: queueName, noAck: true, consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
