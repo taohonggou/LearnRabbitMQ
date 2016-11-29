ac731a98aed4f40fbc95c5a900d437bf4be6e861
taohonggou <cl889521@163.com>
2016-11-29 22:01:43 +0800

    新建了连个项目：routingkey的实践

---
+++ LearnRabbitMQ.Practice/Program.cs
@@ -0,0 +1,14 @@
+﻿using LearnRabbitMQ.Helper;
+
+namespace LearnRabbitMQ.Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RabbitMQHelper.Send("测试消息", "test");
+
+            RabbitMQHelper.Send("测试日志", "log");
+        }
+    }
+}
