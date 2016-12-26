b41420ab64fa59421be593f643bb6889726df2fe
taohonggou <cl889521@163.com>
2016-12-26 22:11:35 +0800

    添加了rpc但是没有写代码

---
+++ LearnRabbitMQ,.RPCClient/RPCClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.MessagePatterns;
+namespace LearnRabbitMQ.RPCClient
+{
+    class _RPCClient
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new RPCClient();
+
+        }
+    }
+}
